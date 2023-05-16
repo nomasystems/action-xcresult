a770a6a71eb4a66056dcae99c661d312705609b3
Jonas Boberg <jonas.boberg@nomasystems.com>
2023-05-16 17:47:32 +0900

    fix: Download and cache xcresult-to-json with actions/tool-cache (#40)
    Download and cache xcresult-to-json with actions/tool-cache rather than expecting
    xcresult-to-json to be pre-installed on the runner.

--- src/xcresult.ts
+++ src/xcresult.ts
@@ -1,5 +1,9 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
+import * as tc from '@actions/tool-cache'
+import * as path from 'path'
+import * as crypto from 'crypto'
+import * as fs from 'fs'
 import {ExecOptions} from '@actions/exec'
 import {Output} from './__generated__/output'
 
@@ -9,7 +13,8 @@ export async function xcresultToJson(
 ): Promise<Output> {
   const args = [xcresultPath].concat(['--path-root', pathRoot])
   const options: ExecOptions = {silent: true}
-  const execOutput = await exec.getExecOutput('xcresult-to-json', args, options)
+  const toolPath = await cachedDownload()
+  const execOutput = await exec.getExecOutput(toolPath, args, options)
   if (execOutput.exitCode === 0) {
     return JSON.parse(execOutput.stdout)
   } else {
@@ -20,4 +25,36 @@ export async function xcresultToJson(
       `xcresult-to-json failed with exit code: ${execOutput.exitCode}`
     )
   }
+}
+
+async function cachedDownload(): Promise<string> {
+  const name = 'xcresult-to-json'
+  const version = '0.2.0'
+  const downloadUrl = `https://github.com/nomasystems/xcresult-to-json/releases/download/${version}/xcresult-to-json.zip`
+  const checksum =
+    '1b44cffc86758237413f3fca49ae9686f3bd65fea60b95cbd1c6d29967cb89da'
+
+  var cachedPath = tc.find(name, version)
+  if (cachedPath) {
+    core.debug(`Found ${name} in cache: ${cachedPath}`)
+  } else {
+    core.debug(`Downloading ${name}`)
+    const downloadPath = await tc.downloadTool(downloadUrl)
+    const downloadChecksum = fileChecksum(downloadPath)
+    if (downloadChecksum !== checksum) {
+      throw new Error(
+        `${downloadPath} checksum mismatch, actual: ${downloadChecksum}, expected: ${checksum}`
+      )
+    }
+    const extractedDir = await tc.extractZip(downloadPath)
+    cachedPath = await tc.cacheDir(extractedDir, name, version)
+  }
+  return path.join(cachedPath, name)
+}
+
+function fileChecksum(filePath: string): string {
+  const fileBuffer = fs.readFileSync(filePath)
+  const hashSum = crypto.createHash('sha256')
+  hashSum.update(fileBuffer)
+  return hashSum.digest('hex')
 }
